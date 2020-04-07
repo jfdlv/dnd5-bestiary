2d1de68e3df93aee4ca423bbf0909f4322ca31c9
Fernando De La Via <jf.delavia@gmail.com>
2020-04-07 10:12:34 -0400

    Updates

---
+++ precache-manifest.478a4bde28b7042963265444307b2aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b913351c54da3edff19b970d0f2f3b95",
+    "url": "/dnd5-bestiary/index.html"
+  },
+  {
+    "revision": "0215405ba31127ce3629",
+    "url": "/dnd5-bestiary/static/css/2.de39f076.chunk.css"
+  },
+  {
+    "revision": "8b41f3a8015ce5d16a11",
+    "url": "/dnd5-bestiary/static/css/main.2a960526.chunk.css"
+  },
+  {
+    "revision": "0215405ba31127ce3629",
+    "url": "/dnd5-bestiary/static/js/2.95c6f170.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/dnd5-bestiary/static/js/2.95c6f170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b41f3a8015ce5d16a11",
+    "url": "/dnd5-bestiary/static/js/main.2046d8ed.chunk.js"
+  },
+  {
+    "revision": "df3183d40f07420818ba",
+    "url": "/dnd5-bestiary/static/js/runtime-main.d4c7c0f7.js"
+  }
+]);
